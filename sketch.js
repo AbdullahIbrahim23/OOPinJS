7e18e6c3e1d82b8ace6f75181a709f5591452e54
AbdullahIbrahim23 <AbdullahIbrahim23@users.noreply.github.com>
2017-10-25 13:38:52 -0400

    xd

--- sketch.js
+++ sketch.js
@@ -1,26 +1,40 @@
-var bubble = {
-	x: 300,
-	y: 200,
-}
+let bubbles = [];
 
 function setup() { // built-in P5.JS function -=- this runs once
 	createCanvas(600, 400); 
 }
 
+function mouseDragged(){
+	let r = random(10, 50);
+	let b = new Bubble(mouseX, mouseY, r)
+	bubbles.push(b);
+}
+
 function draw() { // built-in P5.JS function -=-  automatic loop that repeats forever
 	background(0); // give the canvas a black background
-	move();
-	display();
+	for (let i = 0; i < bubbles.length; i++){
+		bubbles[i].move();
+		bubbles[i].show();	
+	}
+	
 }
 
-function display() {
-	stroke(255); // white outline
-	strokeWeight(4); // line width
-	noFill();
-	ellipse(bubble.x, bubble.y, 24, 24); // draw an ellipse/circle
-}
+class Bubble{
+	constructor(x, y, r){
+		this.x = x;
+		this.y = y;
+		this.r = r;
+	}
+	
+	move(){
+		this.x = this.x + random(-5,5);
+		this.y = this.y + random(-5,5);
+	}
 
-function move() {
-	bubble.x = bubble.x + random(-5,5);
-	bubble.y = bubble.y + random(-5,5);
+	show() {
+		stroke(255);
+		strokeWeight(4);
+		noFill();
+		ellipse(this.x, this.y, this.r*2, this.r*2);
+	}
 }
